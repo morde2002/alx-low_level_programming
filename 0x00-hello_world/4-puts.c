64c534cae445f2c6950e1eaf1bfeb4e5ffdeae83
morde2002 <mathengemordecai@gmail.com>
2023-03-17 01:18:23 -0700

    task 1-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,3 +6,13 @@ int main()
 	puts(str);
 	return (0);
 }
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(str);
+
+	return (0);
+}
