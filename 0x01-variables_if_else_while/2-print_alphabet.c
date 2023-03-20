2ba2d5aca2b5f2ee8a1c5d870174094b9e8a1d35
morde2002 <mathengemordecai@gmail.com>
2023-03-20 01:25:05 -0700

    printing alph in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char str[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+
+int i;
+
+for (i = 0; i < 26; i++)
+
+	{
+
+		putchar(alp[i]);
+
+	}
+
+putchar('\n');
+
+return (0);
+
+}
