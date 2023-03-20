e7b2bbec97ed39595e354e83e5846dfb3edd2765
morde2002 <mathengemordecai@gmail.com>
2023-03-20 01:45:11 -0700

    printing alph in lowercase except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char str[] = "abcdfghijklmnoprstuvwxyz\n";
+
+int i;
+
+for (i = 0; i <= 52; i++)
+{
+
+putchar(str[i]);
+
+}
+
+return (0);
+
+}
