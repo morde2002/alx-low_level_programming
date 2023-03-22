054a75092e7275d01c9ca9b284c8f54027112573
morde2002 <mathengemordecai@gmail.com>
2023-03-22 05:16:19 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,9 +8,7 @@ int main(void)
 {
 	int c, boolean, boolean2;
 	long int n1, n2, fn, fn2, n11, n22;
-	n1 = 1;
-	n2 = 2;
-	boolean =  boolean2 = 1;
+	n1 = 1; n2 = 2; boolean =  boolean2 = 1;
 	printf("%ld, %ld", n1, n2);
 	for (c = 0; c < 96; c++)
 	{
@@ -18,15 +16,13 @@ int main(void)
 		{
 			fn = n1 + n2;
 			printf(", %ld", fn);
-			n1 = n2;
-			n2 = fn;
+			n1 = n2; n2 = fn;
 		}
 		else
 		{
 			if (boolean2)
 			{
-				n11 = n1 % 1000000000;
-				n22 = n2 % 1000000000;
+				n11 = n1 % 1000000000; n22 = n2 % 1000000000;
 				n1 = n1 / 1000000000;
 				n2 = n2 / 1000000000;
 				boolean2 = 0;
