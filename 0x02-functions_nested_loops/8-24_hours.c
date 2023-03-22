2e43c6da712ca0d1c3f2ba7a7049fd027988f04b
morde2002 <mathengemordecai@gmail.com>
2023-03-22 01:49:07 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,46 +8,27 @@
 
 void jack_bauer(void)
 {
-
 	int hrs = 0;
-
 	int mins = 0;
-
 	int rem_mins;
-
 	int rem_hrs;
 
 	while (hrs <= 23)
 	{
-
 		while (mins <= 59)
 		{
-
 			rem_hrs = hrs % 10;
-
 			rem_mins = mins % 10;
-
 			_putchar(hrs / 10 + '0');
-
 			_putchar(rem_hrs + '0');
-
 			_putchar(':');
-
 			_putchar(mins / 10 + '0');
-
 			_putchar(rem_mins + '0');
-
 			mins++;
-
 			_putchar('\n');
-
 		}
-
 		hrs++;
-
 		mins = 0;
-
 	}
-
 }
 
