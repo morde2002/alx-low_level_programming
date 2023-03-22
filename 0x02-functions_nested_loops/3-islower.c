9ddc2f7c587e7d2b538ac1f5d49068385343f3da
morde2002 <mathengemordecai@gmail.com>
2023-03-22 00:23:13 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,16 +8,20 @@
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{	
-		return (1);	
-	}
-	else
-	{
+if (c >= 'a' && c <= 'z')
+{	
 
-		return (0);
+return (1);	
 
-	}
+}
+
+else
+
+{
+
+return (0);
+
+}
 
 }
 
